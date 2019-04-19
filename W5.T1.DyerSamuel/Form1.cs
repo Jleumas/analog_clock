ca3121f9e15b51b3d11bf47bbc3994995a667eb8
Sam Dyer <samueljdyer@gmail.com>
2019-04-18 20:05:55 -0700

    Initial commit

---
+++ W5.T1.DyerSamuel/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W5.T1.DyerSamuel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clockTimer.Start();
+        }
+
+        private void clockTimer_Tick(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(SystemColors.Control);
+            ClockControl clock = new ClockControl(pictureBox1.Width);
+
+            g.DrawLine(clock.returnPen("hour"), clock.Origin, clock.updateHours());
+            g.DrawLine(clock.returnPen("minute"), clock.Origin, clock.updateMinutes());
+            g.DrawLine(clock.returnPen("second"), clock.Origin, clock.updateSeconds());
+            //label1.Text = DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString() + ":" + DateTime.Now.Second.ToString();     //for testing
+        }
+    }
+}
---
+++ W5.T1.DyerSamuel/clockControl.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace W5.T1.DyerSamuel
+{
+    class ClockControl
+    {
+        public ClockControl(int canvasSize)
+        {
+            secondHandLength = (int)(canvasSize * 0.5 * 0.9F);
+            minuteHandLength = (int)(canvasSize * 0.5 * 0.6F);
+            hourHandLength = (int)(canvasSize * 0.5 * 0.35F);
+
+            Origin.X = canvasSize / 2;
+            Origin.Y = canvasSize / 2;
+        }
+
+        public int secondHandLength;
+        public int minuteHandLength;
+        public int hourHandLength;
+
+        public Point Origin;
+
+        public Pen returnPen(string timeType)
+        {
+            float width;
+            Color color;
+            switch (timeType)
+            {
+                case "hour":
+                    width = 8F;
+                    color = Color.Black;
+                    break;
+
+                case "minute":
+                    width = 5F;
+                    color = Color.Blue;
+                    break;
+
+                case "second":
+                    width = 2F;
+                    color = Color.Red;
+                    break;
+
+                default:
+                    width = 0F;
+                    color = Color.Black;
+                    break;
+            }
+            Pen pen = new Pen(color, width);
+            return pen;
+        }
+
+        public Point handPosition(int angle, int radius)
+        {
+            Point point = new Point();
+            point.X = (int)(Origin.X + (radius * Math.Cos(degreesToRadians(angle))));
+            point.Y = (int)(Origin.Y + (radius * Math.Sin(degreesToRadians(angle))));
+            return point;
+        }
+
+        public Point updateSeconds()
+        {
+            return handPosition((270 + (DateTime.Now.Second * 6)), secondHandLength);      //one second per six degrees;
+        }
+
+        public Point updateMinutes()
+        {
+            return handPosition((270 + (DateTime.Now.Minute * 6)), minuteHandLength);      //one minute per six degrees;
+        }
+
+        public Point updateHours()
+        {
+            return handPosition((270 + (DateTime.Now.Hour * 30)), hourHandLength);     //one hour per 30 degrees;
+        }
+
+        public double degreesToRadians(int angle)
+        {
+            double radians = angle * Math.PI / 180; //degrees * radians per degree
+            return radians;
+        }
+    }
+}
